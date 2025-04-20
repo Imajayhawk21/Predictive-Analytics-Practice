ba802d014cdb6c420b3fbdd61f0f69c0d2a3c6d0
Imajayhawk21 <157774916+Imajayhawk21@users.noreply.github.com>
2025-04-19 23:06:54 -0500

    Update Linear_Regression_Easy.r

--- Linear_Regression_Easy.r
+++ Linear_Regression_Easy.r
@@ -11,3 +11,13 @@ head(income_data)
 
 # Check the structure of the dataframe
 summary(income_data)
+
+# Create histogram of happiness scores
+hist(income_data$happiness)
+# Create a scatter plot of happiness vs income
+plot(happiness ~ income, data = income_data)
+
+# Fit a linear regression model
+model <- lm(happiness ~ income, data = income_data)
+# Print the summary of the model
+summary(model)
