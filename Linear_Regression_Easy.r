a453abb1989ed6215d89108d912a35b6d29762fa
Imajayhawk21 <157774916+Imajayhawk21@users.noreply.github.com>
2025-05-19 11:57:35 -0500

    Update Linear_Regression_Easy.r

--- Linear_Regression_Easy.r
+++ Linear_Regression_Easy.r
@@ -21,3 +21,26 @@ plot(happiness ~ income, data = income_data)
 model <- lm(happiness ~ income, data = income_data)
 # Print the summary of the model
 summary(model)
+
+par(mfrow=c(2,2))
+plot(model)
+
+# Create a ggplot scatter plot of happiness vs income
+income.graph <- ggplot(income_data, aes(x = income, y = happiness)) +
+                geom_point()
+income.graph
+
+income.graph <- income.graph + geom_smooth(method="lm", col="black")
+
+income.graph
+
+income.graph <- income.graph +
+  stat_regline_equation(label.x = 3, label.y = 7)
+
+income.graph
+
+income.graph +
+  theme_bw() +
+  labs(title = "Reported happiness as a function of income",
+      x = "Income (x$10,000)",
+      y = "Happiness score (0 to 10)")
